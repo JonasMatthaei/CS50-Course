257582e9daa1dc91edaf40001a71935f3c6d6ac5
JonasMatthaei <78216366+JonasMatthaei@users.noreply.github.com>
2022-01-16 12:24:35 +0100

    Update ProblemSet2.c

--- Lectures/ProblemSet2.c
+++ Lectures/ProblemSet2.c
@@ -4,15 +4,6 @@
 #include <string.h>
 #include <ctype.h>
 
-/**
- * Caesar.c
- * A program that encrypts messages using Caesar’s cipher. Your program must
- * accept a single command-line argument: a non-negative integer. Let’s call it
- * k for the sake of discussion. If your program is executed without any
- * command-line arguments or with more than one command-line argument, your
- * program should yell at the user and return a value of 1.
- * 
- * */
  
  int main(int argc, string argv[])
  {
